727227a59943bb26a8605dda1fafec0bb286c23d
justToLearnNExplore <pranusha11201@gmail.com>
2023-11-26 23:34:51 +0530

    authenticate users & collect data

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,25 @@ module.exports.profile = function(req, res){
         title:"UserProfile",
     });
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up",
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In",
+    });
+}
+
+module.exports.create = function(req, res){
+    
+}
+
+module.exports.createSession = function(req, res){
+    
+}
+
+
