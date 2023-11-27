6e4dc37b4eb369df66dce5ee5bc82bf9fc2888d9
justToLearnNExplore <pranusha11201@gmail.com>
2023-11-27 16:13:32 +0530

    handle cookies and get user data on sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     res.render('user_profile',{
         title:"UserProfile",
@@ -16,10 +18,31 @@ module.exports.signIn = function(req, res){
     });
 }
 
+//get signup data
 module.exports.create = function(req, res){
+    if(req.body.password != req.body.comfirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in signing up the user');
+            return;
+        }
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in signing up the user');
+                    return; 
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
     
 }
-
+//signin & create a session for user
 module.exports.createSession = function(req, res){
     
 }
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,8 @@ router.get('/profile', userController.profile);
 router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
 
+router.post('/create', userController.create);
+
+
 
 module.exports = router;
