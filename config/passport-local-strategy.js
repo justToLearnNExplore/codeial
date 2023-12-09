fd1fac92ab34d15888d70b2c9e5c3424e03f72f4
justToLearnNExplore <pranusha11201@gmail.com>
2023-12-09 16:05:05 +0530

    add nsm i.e. scss

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,102 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+// passport.use(new LocalStrategy({
+//     usernameField: 'email'
+// },
+// function(email, password, done){
+//     //find user & establish identity
+//     User.findOne({email: email}, function(err, user){
+//          if(err){
+//             console.log('Error in finding user --> Passport');
+//             return done(err);
+//          }
+
+//          if(!user || user.password != password){
+//             console.log('Invalid Username/Password');
+//             return done(null, false);
+//          }
+//          return done(null, user);
+//     });
+// }
+// ));
+
+//serializing the user to decide which key is to be kept in the cookies
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+},
+async function(email, password, done){
+    try {
+        // Find user by email
+        const user = await User.findOne({ email });
+
+        if (!user || user.password !== password) {
+            console.log('Invalid Username/Password');
+            return done(null, false);
+        }
+
+        return done(null, user);
+    } catch (err) {
+        console.log('Error in finding user --> Passport', err);
+        return done(err);
+    }
+}
+));
+
+
+passport.serializeUser(function(user, done){
+     done(null, user.id);
+});
+
+//deserializing the user from the key in the cookies
+passport.deserializeUser(async function(id, done){
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            console.log('User not found');
+            return done(null, false);
+        }
+
+        return done(null, user);
+    } catch (err) {
+        console.log('Error in finding user --> Passport', err);
+        return done(err);
+    }
+});
+
+
+// passport.deserializeUser(function(id, done){
+//       User.findById(id, function(err, user){
+//           if(err){
+//             console.log('Error in finding user --> Passport');
+//             return done(err);
+//           }
+//           return done(null, user);
+//       });
+// });
+
+
+passport.checkAuthentication = function(req, res, next){
+    //if the user is signed in
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    //if user is not signed in
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated()){
+        //req.user contains current signed in user from session cookies & we are sending this to locals for views
+        res.locals.user = req.user;
+    }
+    next();
+}
+
+module.exports = passport;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,44 +7,80 @@ module.exports.profile = function(req, res){
 }
 
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/sign-in');
+    }
     return res.render('user_sign_up',{
         title:"Codeial | Sign Up",
     });
 }
 
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/sign-in');
+    }
     return res.render('user_sign_in',{
         title:"Codeial | Sign In",
     });
 }
 
 //get signup data
-module.exports.create = function(req, res){
-    if(req.body.password != req.body.comfirm_password){
-        return res.redirect('back');
-    }
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log('error in signing up the user');
-            return;
-        }
-        if(!user){
-            User.create(req.body, function(err, user){
-                if(err){
-                    console.log('error in signing up the user');
-                    return; 
-                }
-                return res.redirect('/users/sign-in');
-            })
-        }else{
+module.exports.create = async function (req, res) {
+    if (req.body.password !== req.body.confirm_password) {
+        // Sending a response to the client with an alert
+        return res.status(400).send('<script>alert("Passwords do not match"); window.history.back();</script>');
+     }
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+
+        if (existingUser) {
             return res.redirect('back');
         }
+
+        const newUser = await User.create(req.body);
+        return res.redirect('/users/sign-in');
+    } catch (error) {
+        console.error('Error in signing up the user:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+};
+// module.exports.create = function(req, res){
+//     if(req.body.password != req.body.confirm_password){
+//         return res.redirect('back');
+//     }
+//     User.findOne({email: req.body.email}, function(err, user){
+//         if(err){
+//             console.log('error in signing up the user');
+//             return;
+//         }
+//         if(!user){
+//             User.create(req.body, function(err, user){
+//                 if(err){
+//                     console.log('error in signing up the user');
+//                     return; 
+//                 }
+//                 return res.redirect('/users/sign-in');
+//             })
+//         }else{
+//             return res.redirect('back');
+//         }
+//     });
+    
+// }
+
+
+//signin & create a session for user
+module.exports.createSession = function(req, res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout(function(err) {
+        if (err) {
+            console.log('Error in logging out:', err);
+            return res.redirect('/'); // Redirect to a particular page on error
+        }
+        return res.redirect('/users/sign-in'); // Redirect to a login page or another appropriate page on successful logout
     });
     
 }
-//signin & create a session for user
-module.exports.createSession = function(req, res){
-    
-}
-
-
