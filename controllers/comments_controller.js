7dd2587685555ffeec3235ecadfa049a5bba6b1c
justToLearnNExplore <pranusha11201@gmail.com>
2023-12-10 23:34:16 +0530

    delete posts & also delete comments associated with it

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,31 +24,26 @@ module.exports.create = async function (req, res) {
     return res.redirect('back');
 };
 
+module.exports.destroy = async function (req, res) {
+    try {
+        const comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+
+            await comment.deleteOne({ _id: req.params.id });
+
+            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+
+            return res.redirect('back');
+        } else {
+            return res.status(403).send('Unauthorized');
+        }
+
+    } catch (error) {
+        console.error('Error in deleting the comment', error);
+        return res.status(500).send('Internal Server Error');
+    }
+};
 
 
-// const Comment = require('../models/comment');
-// const Post = require('../models/post');
-
-// module.exports.create = async function (req, res) {
-//     try {
-//         const newPost = await Post.findById(req.body.post, function(err, post){
-//             if(post){
-//                 Comment.create({
-//                     content: req.body.content,
-//                     user: req.user._id,
-//                     post: req.body.post
-//                 },function(err, comment){
-//                      post.comments.push(comment);
-//                      post.save();
-//                      res.redirect('/');
-//                 })
-//             }
-            
-//         });
-       
-//     } catch (error) {
-//         console.error('Error in creating the post', error);
-//         return res.status(500).send('Internal Server Error');
-//     }
-//     return res.redirect('back');
-// };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function (req, res) {
     try {
@@ -12,4 +13,30 @@ module.exports.create = async function (req, res) {
         return res.status(500).send('Internal Server Error');
     }
     return res.redirect('back');
+};
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+
+        if(post.user == req.user.id){
+            // await post.remove();
+            await Post.deleteOne({ _id: req.params.id });
+
+            // Delete associated comments
+            await Comment.deleteMany({ post: req.params.id });
+            
+            return res.redirect('back');
+        } else {
+            return res.status(403).send('Unauthorized'); // Or handle unauthorized access
+        }
+       
+    } catch (error) {
+        console.error('Error in deleting the post', error);
+        return res.status(500).send('Internal Server Error');
+    }
 };
